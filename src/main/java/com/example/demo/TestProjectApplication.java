65cc4fdb25223f93f132ff293fd84adc121f7ce2
Abhi1494 <94425094+Abhi1494@users.noreply.github.com>
2021-11-25 12:40:13 +0530

    Update TestProjectApplication.java
    multiply method is added

--- src/main/java/com/example/demo/TestProjectApplication.java
+++ src/main/java/com/example/demo/TestProjectApplication.java
@@ -9,10 +9,13 @@ public class TestProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
 		System.out.println("add: "+add(2,5));
+		System.out.println("multiply: "+multiply(7,5));
 	}
 
 	public static int add(int a,int b) {
 		return a+b;
 	}
-	
+	public static int multiply(int a,int b){
+		return a*b;
+	}
 }
