780d7ff423225e70af4bdc1a76c592bfcd345f6f
Abhishek Singh <abhisheksinghnagwansi@gmail.com>
2021-11-25 12:29:12 +0530

    create add method in java class

--- src/main/java/com/example/demo/TestProjectApplication.java
+++ src/main/java/com/example/demo/TestProjectApplication.java
@@ -8,6 +8,11 @@ public class TestProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
+		System.out.println("add: "+add(2,5));
 	}
 
+	public static int add(int a,int b) {
+		return a+b;
+	}
+	
 }
